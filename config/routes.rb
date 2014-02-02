f667c01a610063984ca55f1d0d7dadfba35297be
Elena Posea <elena.posea@gmail.com>
2014-02-02 15:17:52 +0200

    modificat header

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ GangOfFour::Application.routes.draw do
   root :to => "static_pages#home"
   match 'home', to: 'static_pages#home', via: :get
   match 'about', to: 'static_pages#about', via: :get
+
   resources :petitions
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
