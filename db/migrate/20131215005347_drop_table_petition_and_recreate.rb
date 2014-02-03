14c42bbe93c457be87b4b4f8060710e7622d4ca6
Daniela <daniela@ubuntu.(none)>
2014-02-04 01:21:33 +0200

    Am rulat migratii.

--- db/migrate/20131215005347_drop_table_petition_and_recreate.rb
+++ db/migrate/20131215005347_drop_table_petition_and_recreate.rb
@@ -1,6 +1,6 @@
 class DropTablePetitionAndRecreate < ActiveRecord::Migration
   def change
-     drop_table :system_settings
+     
       create_table :petition do |t|
       t.string  :titlu
       t.string  :continut
