9aba1dc1b854fb82fa7cabba9676391604528e76
Elena Posea <elena.posea@gmail.com>
2014-02-03 12:56:59 +0200

    merge bine, fara tagare #11

--- app/models/petition.rb
+++ app/models/petition.rb
@@ -1,6 +1,5 @@
 class Petition < ActiveRecord::Base
-  # attr_accessible :titlu, :continut, :tag_list
-  acts_as_taggable_on :tag_list  
+  # acts_as_taggable_on :tag_list  
   belongs_to :user
 end
 
